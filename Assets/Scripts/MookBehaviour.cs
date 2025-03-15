39c516ce7cbb2839bece881f16c134aa0936c89a
JMFStorm <jannematiasf@gmail.com>
2025-03-15 12:50:23 +0200

    mook states1

--- Assets/Scripts/MookBehaviour.cs
+++ Assets/Scripts/MookBehaviour.cs
@@ -3,10 +3,11 @@ using UnityEngine;
 
 public enum EnemyState
 {
-    Normal = 0,
+    Passive = 0,
     Alert,
     AttackMoving,
-    Attacking
+    Attacking,
+    NormalMoving,
 }
 
 [RequireComponent(typeof(BoxCollider2D))]
@@ -31,6 +32,13 @@ public class MookBehaviour : MonoBehaviour
     private EnemyState _state;
 
     private bool _facingLeft = false;
+
+    private enum CollisionTypes
+    {
+        None = 0,
+        GroundEdge,
+        WallHit
+    }
 
     void Awake()
     {
@@ -65,14 +73,31 @@ public class MookBehaviour : MonoBehaviour
 
     void Start()
     {
+        _state = EnemyState.NormalMoving;
         StartCoroutine(MovementLoop());
     }
 
     void Update()
     {
         TryNormalMovement();
-
         TryAggroMovement();
+
+        DetectPlayerAndAggro();
+
+        _spriteRenderer.color = GetEnemyStateColor(_state);
+    }
+
+    Color GetEnemyStateColor(EnemyState state)
+    {
+        return state switch
+        {
+            EnemyState.NormalMoving => Color.green,
+            EnemyState.Alert => Color.yellow,
+            EnemyState.AttackMoving => Color.red,
+            EnemyState.Attacking => Color.blue,
+            EnemyState.Passive => Color.gray,
+            _ => throw new System.NotImplementedException(),
+        };
     }
 
     void TryAggroMovement()
@@ -82,8 +107,6 @@ public class MookBehaviour : MonoBehaviour
             return;
         }
 
-        Debug.Log("ATTACK moving!");
-
         var distanceFromTarget = Mathf.Abs(transform.position.x - _aggroTarget.x);
 
         if (distanceFromTarget <= AttackRange)
@@ -92,33 +115,48 @@ public class MookBehaviour : MonoBehaviour
         }
         else
         {
+            var collisions = GetRaycastCollisions();
+
+            if (collisions == CollisionTypes.GroundEdge || collisions == CollisionTypes.WallHit)
+            {
+                Debug.Log("Aggro ended on collision " + Time.time);
+
+                // NOTE: End aggro on collisions
+                StartCoroutine(ResetAndTurnAround(1.5f));
+                return;
+            }
+
             float newMovement = _facingLeft ? -AggroSpeed : AggroSpeed;
             _rigidBody.linearVelocity = new Vector2(newMovement, _rigidBody.linearVelocity.y);
         }
     }
 
+    IEnumerator ResetAndTurnAround(float passiveLength)
+    {
+        _state = EnemyState.Passive;
+
+        yield return new WaitForSeconds(passiveLength);
+
+        TurnAround();
+
+        _state = EnemyState.NormalMoving;
+    }
+
     IEnumerator Attack()
     {
-        Debug.Log("ATTACK!");
-
         _state = EnemyState.Attacking;
 
-        yield return 1.0f;
+        yield return new WaitForSeconds(1.0f);
 
         _spriteRenderer.color = Color.white;
-        _state = EnemyState.Normal;
+        _state = EnemyState.NormalMoving;
 
         StartCoroutine(MovementLoop());
     }
 
-    void TryNormalMovement()
+    CollisionTypes GetRaycastCollisions()
     {
-        if (_state != EnemyState.Normal)
-        {
-            return;
-        }
-
-        var direction = _facingLeft ? -1f : 1f;
+        var direction = GetXDirection();
 
         const float groundRayLength = 0.25f;
         RaycastHit2D groundHit = Physics2D.Raycast(_groundCheck.position, Vector2.down, groundRayLength, _groundFloorLayer);
@@ -129,36 +167,67 @@ public class MookBehaviour : MonoBehaviour
         RaycastHit2D wallHit = Physics2D.Raycast(transform.position, Vector2.right * direction, wallRayLength, _wallLayer);
         Debug.DrawRay(transform.position, direction * wallRayLength * Vector2.right, wallHit.collider ? Color.magenta : Color.cyan);
 
-        if (!groundHit.collider || wallHit.collider)
+        if (wallHit.collider)
         {
-            TurnAround();
+            return CollisionTypes.WallHit;
         }
 
+        if (!groundHit.collider)
         {
-            const float detectionDistance = 6.0f;
-            Vector2 detectionBoxSize = new(detectionDistance, 1.5f);
+            return CollisionTypes.GroundEdge;
+        }
 
-            var detectionOffset = (detectionDistance * 0.25f) * Vector2.right;
-            Vector2 boxPosition = (Vector2)transform.position + (_facingLeft ? -detectionOffset : detectionOffset);
+        return CollisionTypes.None;
+    }
 
-            Collider2D hit = Physics2D.OverlapBox(boxPosition, detectionBoxSize, 0f, _playerLayer);
+    float GetXDirection()
+    {
+        return _facingLeft ? -1f : 1f;
+    }
 
-            if (hit != null && hit.CompareTag("Player"))
-            {
-                _aggroTarget = hit.gameObject.transform.position;
-                StartCoroutine(ActivateAggro());
+    void DetectPlayerAndAggro()
+    {
+        if (_state != EnemyState.NormalMoving)
+        {
+            return;
+        }
 
-                DebugUtil.DrawRectangle(boxPosition, detectionBoxSize, Color.red);
-                Debug.Log($"_aggroTarget: {_aggroTarget}");
-            }
-            else
-            {
-                DebugUtil.DrawRectangle(boxPosition, detectionBoxSize, Color.green);
-            }
+        const float detectionDistance = 6.0f;
+        Vector2 detectionBoxSize = new(detectionDistance, 1.5f);
+
+        var detectionOffset = (detectionDistance * 0.25f) * Vector2.right;
+        Vector2 boxPosition = (Vector2)transform.position + (_facingLeft ? -detectionOffset : detectionOffset);
+
+        Collider2D hit = Physics2D.OverlapBox(boxPosition, detectionBoxSize, 0f, _playerLayer);
+
+        if (hit != null && hit.CompareTag("Player"))
+        {
+            _aggroTarget = hit.gameObject.transform.position;
+            StartCoroutine(ActivateAggro());
+            DebugUtil.DrawRectangle(boxPosition, detectionBoxSize, Color.red);
+        }
+        else
+        {
+            DebugUtil.DrawRectangle(boxPosition, detectionBoxSize, Color.green);
+        }
+    }
+
+    void TryNormalMovement()
+    {
+        if (_state != EnemyState.NormalMoving)
+        {
+            return;
+        }
+
+        var collisions = GetRaycastCollisions();
+
+        if (collisions == CollisionTypes.GroundEdge || collisions == CollisionTypes.WallHit)
+        {
+            StartCoroutine(ResetAndTurnAround(1.0f));
         }
 
         float newMovement = _facingLeft ? -MovementSpeed : MovementSpeed;
-        Debug.DrawRay(_groundCheck.position, Vector2.right * direction, _facingLeft ? Color.green : Color.red);
+        Debug.DrawRay(_groundCheck.position, Vector2.right * GetXDirection(), _facingLeft ? Color.green : Color.red);
         _rigidBody.linearVelocity = new Vector2(newMovement, _rigidBody.linearVelocity.y);
     }
 
@@ -166,7 +235,13 @@ public class MookBehaviour : MonoBehaviour
     {
         var targetDir = transform.position.x - _aggroTarget.x;
 
-        _facingLeft = 0 < targetDir;
+        var targetDirRight = targetDir < 0f;
+        var facingXdir = GetXDirection();
+
+        if (targetDirRight && facingXdir < 0f || !targetDirRight && 0f < facingXdir)
+        {
+            TurnAround();
+        }
 
         _spriteRenderer.color = Color.yellow;
         _state = EnemyState.Alert;
@@ -175,6 +250,17 @@ public class MookBehaviour : MonoBehaviour
 
         _spriteRenderer.color = Color.red;
         _state = EnemyState.AttackMoving;
+        StartCoroutine(AttackMoveMaxTimer());
+    }
+
+    IEnumerator AttackMoveMaxTimer()
+    {
+        yield return new WaitForSeconds(3.5f);
+
+        if (_state == EnemyState.AttackMoving)
+        {
+            _state = EnemyState.NormalMoving;
+        }
     }
 
     IEnumerator MovementLoop()
@@ -184,7 +270,7 @@ public class MookBehaviour : MonoBehaviour
             float moveTime = Random.Range(2f, 8f);
             yield return new WaitForSeconds(moveTime);
 
-            if (_state != EnemyState.Normal)
+            if (_state != EnemyState.NormalMoving)
             {
                 yield return null;
             }
@@ -192,7 +278,7 @@ public class MookBehaviour : MonoBehaviour
             float waitTime = Random.Range(2f, 4f);
             yield return new WaitForSeconds(waitTime);
 
-            if (_state != EnemyState.Normal)
+            if (_state != EnemyState.NormalMoving)
             {
                 yield return null;
             }
@@ -208,9 +294,9 @@ public class MookBehaviour : MonoBehaviour
     {
         _facingLeft = !_facingLeft;
 
+        Debug.Log("Enemy turn around" + Time.time);
+
         var newCheckerX = _groundCheck.localPosition.x * -1;
         _groundCheck.localPosition = new(newCheckerX, _groundCheck.localPosition.y, _groundCheck.localPosition.z);
-
-        Debug.Log($"Turn around {gameObject.name} to left: {_facingLeft}");
     }
 }
