18013eb114aec60948ea1ef7eb8b476f28c052e3
JMFStorm <jannematiasf@gmail.com>
2025-03-15 10:13:15 +0200

    mook aggro1

--- Assets/Scripts/MookBehaviour.cs
+++ Assets/Scripts/MookBehaviour.cs
@@ -1,20 +1,34 @@
 using System.Collections;
 using UnityEngine;
 
+public enum EnemyState
+{
+    Normal = 0,
+    Alert,
+    AttackMoving,
+    Attacking
+}
+
 [RequireComponent(typeof(BoxCollider2D))]
 [RequireComponent(typeof(Rigidbody2D))]
 public class MookBehaviour : MonoBehaviour
 {
     public float MovementSpeed = 1.5f;
-    public Transform GroundCheck;
+    public float AggroSpeed = 3.0f;
+    public float AttackRange = 1.0f;
 
+    private Transform _groundCheck;
     private BoxCollider2D _boxCollider;
     private Rigidbody2D _rigidBody;
-    
+    private SpriteRenderer _spriteRenderer;
+
+    private Vector2 _aggroTarget;
+
     private LayerMask _groundFloorLayer;
     private LayerMask _wallLayer;
+    private LayerMask _playerLayer;
 
-    private bool _isMoving = false;
+    private EnemyState _state;
 
     private bool _facingLeft = false;
 
@@ -32,15 +46,21 @@ public class MookBehaviour : MonoBehaviour
 
         _groundFloorLayer = LayerMask.GetMask("Ground", "Platform");
         _wallLayer = LayerMask.GetMask("Ground", "DamageZone");
+        _playerLayer = LayerMask.GetMask("Character");
 
-        GroundCheck = transform.Find("GroundCheck");
+        _groundCheck = transform.Find("GroundCheck");
 
-        if (GroundCheck == null)
+        if (_groundCheck == null)
         {
             Debug.LogError($"GroundCheck not found on {nameof(MookBehaviour)}");
         }
 
-        Debug.Log($"GroundCheck {GroundCheck.localPosition}");
+        var sprite = transform.Find("Sprite");
+
+        if (!sprite.TryGetComponent(out _spriteRenderer))
+        {
+            Debug.LogError($"{nameof(SpriteRenderer)} not found on child of {nameof(MookBehaviour)}");
+        }
     }
 
     void Start()
@@ -50,12 +70,50 @@ public class MookBehaviour : MonoBehaviour
 
     void Update()
     {
-        TryMove();
+        TryNormalMovement();
+
+        TryAggroMovement();
     }
 
-    void TryMove()
+    void TryAggroMovement()
     {
-        if (!_isMoving)
+        if (_state != EnemyState.AttackMoving)
+        {
+            return;
+        }
+
+        Debug.Log("ATTACK moving!");
+
+        var distanceFromTarget = Mathf.Abs(transform.position.x - _aggroTarget.x);
+
+        if (distanceFromTarget <= AttackRange)
+        {
+            StartCoroutine(Attack());
+        }
+        else
+        {
+            float newMovement = _facingLeft ? -AggroSpeed : AggroSpeed;
+            _rigidBody.linearVelocity = new Vector2(newMovement, _rigidBody.linearVelocity.y);
+        }
+    }
+
+    IEnumerator Attack()
+    {
+        Debug.Log("ATTACK!");
+
+        _state = EnemyState.Attacking;
+
+        yield return 1.0f;
+
+        _spriteRenderer.color = Color.white;
+        _state = EnemyState.Normal;
+
+        StartCoroutine(MovementLoop());
+    }
+
+    void TryNormalMovement()
+    {
+        if (_state != EnemyState.Normal)
         {
             return;
         }
@@ -63,9 +121,9 @@ public class MookBehaviour : MonoBehaviour
         var direction = _facingLeft ? -1f : 1f;
 
         const float groundRayLength = 0.25f;
-        RaycastHit2D groundHit = Physics2D.Raycast(GroundCheck.position, Vector2.down, groundRayLength, _groundFloorLayer);
+        RaycastHit2D groundHit = Physics2D.Raycast(_groundCheck.position, Vector2.down, groundRayLength, _groundFloorLayer);
 
-        Debug.DrawRay(GroundCheck.position, Vector2.down * groundRayLength, Color.green);
+        Debug.DrawRay(_groundCheck.position, Vector2.down * groundRayLength, Color.green);
 
         float wallRayLength = (_boxCollider.size.x / 2) + 0.1f;
         RaycastHit2D wallHit = Physics2D.Raycast(transform.position, Vector2.right * direction, wallRayLength, _wallLayer);
@@ -76,22 +134,68 @@ public class MookBehaviour : MonoBehaviour
             TurnAround();
         }
 
+        {
+            const float detectionDistance = 6.0f;
+            Vector2 detectionBoxSize = new(detectionDistance, 1.5f);
+
+            var detectionOffset = (detectionDistance * 0.25f) * Vector2.right;
+            Vector2 boxPosition = (Vector2)transform.position + (_facingLeft ? -detectionOffset : detectionOffset);
+
+            Collider2D hit = Physics2D.OverlapBox(boxPosition, detectionBoxSize, 0f, _playerLayer);
+
+            if (hit != null && hit.CompareTag("Player"))
+            {
+                _aggroTarget = hit.gameObject.transform.position;
+                StartCoroutine(ActivateAggro());
+
+                DebugUtil.DrawRectangle(boxPosition, detectionBoxSize, Color.red);
+                Debug.Log($"_aggroTarget: {_aggroTarget}");
+            }
+            else
+            {
+                DebugUtil.DrawRectangle(boxPosition, detectionBoxSize, Color.green);
+            }
+        }
+
         float newMovement = _facingLeft ? -MovementSpeed : MovementSpeed;
-        Debug.DrawRay(GroundCheck.position, Vector2.right * direction, _facingLeft ? Color.green : Color.red);
+        Debug.DrawRay(_groundCheck.position, Vector2.right * direction, _facingLeft ? Color.green : Color.red);
         _rigidBody.linearVelocity = new Vector2(newMovement, _rigidBody.linearVelocity.y);
+    }
+
+    IEnumerator ActivateAggro()
+    {
+        var targetDir = transform.position.x - _aggroTarget.x;
+
+        _facingLeft = 0 < targetDir;
+
+        _spriteRenderer.color = Color.yellow;
+        _state = EnemyState.Alert;
+
+        yield return new WaitForSeconds(0.5f);
+
+        _spriteRenderer.color = Color.red;
+        _state = EnemyState.AttackMoving;
     }
 
     IEnumerator MovementLoop()
     {
-        while (true) 
+        while (true)
         {
             float moveTime = Random.Range(2f, 8f);
-            _isMoving = true;
             yield return new WaitForSeconds(moveTime);
 
+            if (_state != EnemyState.Normal)
+            {
+                yield return null;
+            }
+
             float waitTime = Random.Range(2f, 4f);
-            _isMoving = false;
             yield return new WaitForSeconds(waitTime);
+
+            if (_state != EnemyState.Normal)
+            {
+                yield return null;
+            }
 
             if (0.75f < Random.value)
             {
@@ -104,8 +208,8 @@ public class MookBehaviour : MonoBehaviour
     {
         _facingLeft = !_facingLeft;
 
-        var newCheckerX = GroundCheck.localPosition.x * -1;
-        GroundCheck.localPosition = new(newCheckerX, GroundCheck.localPosition.y, GroundCheck.localPosition.z);
+        var newCheckerX = _groundCheck.localPosition.x * -1;
+        _groundCheck.localPosition = new(newCheckerX, _groundCheck.localPosition.y, _groundCheck.localPosition.z);
 
         Debug.Log($"Turn around {gameObject.name} to left: {_facingLeft}");
     }
