7521b5626cd0f9550ee1984089594912bfb5d619
JMFStorm <jannematiasf@gmail.com>
2025-04-09 12:22:10 +0300

    gamma slider

--- Assets/Scripts/SettingsSliders.cs
+++ Assets/Scripts/SettingsSliders.cs
@@ -1,28 +1,57 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.Rendering;
 using UnityEngine.UI;
 
 public class SettingsSliders : MonoBehaviour
 {
     public Slider VolumeSlider;
-
     public TextMeshProUGUI VolumeText;
+
+    public Volume PostProcessVolume;
+    public Slider GammaSlider;
+    public TextMeshProUGUI GammaText;
+
+    private ColorAdjustments _colorAdjustments;
 
     void Start()
     {
         float savedVolume = PlayerPrefs.GetFloat("MasterVolume", 0.8f);
         VolumeSlider.value = savedVolume;
         SetVolume(savedVolume);
+
+        PostProcessVolume.profile.TryGet(out _colorAdjustments);
+
+        float savedGamma = PlayerPrefs.GetFloat("Gamma", 0.5f);
+        GammaSlider.value = savedGamma;
+        SetGamma(savedGamma);
     }
 
     public void SetVolume(float value)
     {
         AudioListener.volume = value;
 
-        //var volume = Mathf.Log10(Mathf.Clamp(value, 0.0001f, 1f)) * 20; // Mixer expects volume in dB. 0.0001 avoids log(0).
-        //AudioMixer.SetFloat("MasterVolume", volume);
         PlayerPrefs.SetFloat("MasterVolume", value);
 
-        VolumeText.text = "Volume: " + value.ToString("F2");
+        VolumeText.text = "Volume: " + value.ToString("F3");
+    }
+
+    public void SetGamma(float value)
+    {
+        if (_colorAdjustments != null)
+        {
+            var newValue = Mathf.Lerp(-3f, 3f, value);
+
+            if (-0.15f < newValue && newValue < 0.15f)
+            {
+                newValue = 0f;
+            }
+
+            _colorAdjustments.postExposure.value = newValue;
+            PlayerPrefs.SetFloat("Gamma", value);
+
+            GammaText.text = "Gamma adjustment: " + newValue.ToString("F3");
+        }
     }
 }
