5fe5e67c882a21a0c17fca1cbc21153f5074e8b7
JMFStorm <jannematiasf@gmail.com>
2025-04-24 09:28:02 +0300

    voices 02

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -453,6 +453,11 @@ public class PlayerCharacter : MonoBehaviour
 
     private void OnTriggerStay2D(Collider2D other)
     {
+        if (other.CompareTag("Spikes") && _hasAttackDamageInvulnerability)
+        {
+            return;
+        }
+
         if (other.gameObject.layer == LayerMask.NameToLayer("DamageZone") || other.gameObject.layer == LayerMask.NameToLayer("EnvDamageZone"))
         {
             Vector2 collisionDirection = (transform.position - other.transform.position).normalized;
